f7530fc4af5446367bc416ad98155bd18495a96e
shixian <1145024061@qq.com>
2020-04-10 17:19:05 +0800

    增加扫码分享书评

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,47 +3,37 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    showToolbar:false,
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
+    
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  },
+  //   wx.getSetting({
+  //     success: res => {
+  //       if (res.authSetting['scope.userInfo']) {
+  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+  //         wx.getUserInfo({
+  //           success: res => {
+  //             this.setData({
+  //               avatarUrl: res.userInfo.avatarUrl,
+  //               userInfo: res.userInfo
+  //             })
+  //           }
+  //         })
+  //       }
+  //     }
+  //   })
+  // },
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
+  // onGetUserInfo: function(e) {
+  //   if (!this.data.logged && e.detail.userInfo) {
+  //     this.setData({
+  //       logged: true,
+  //       avatarUrl: e.detail.userInfo.avatarUrl,
+  //       userInfo: e.detail.userInfo
+  //     })
+  //   }
   },
 
   onGetOpenid: function() {
@@ -114,6 +104,31 @@ Page({
       fail: e => {
         console.error(e)
       }
+    })
+  },
+  showBook(){
+    wx.redirectTo({
+      url: '/pages/bookList/bookList',
+    })
+  },
+  onAddBook(){
+    wx.navigateTo({
+      url: '/pages/addBook/addBook',
+    })
+  },
+  onToggle() {
+    this.setData({
+      showToolbar: !this.data.showToolbar,
+    });
+  },
+  onAdd() {
+    wx.redirectTo({
+      url: '/pages/comment/comment',
+    })
+  },
+  onList() {
+    wx.navigateTo({
+      url: '/pages/list/list',
     })
   },
 
